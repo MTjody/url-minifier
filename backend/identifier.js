997f40bed713e8cc0c69d9bde10881bb898f172b
MTJody <myad.tahajody@gmail.com>
2020-02-22 22:33:31 +0100

    Clear cut between backend / frontend

--- backend/identifier.js
+++ backend/identifier.js
@@ -1,7 +1,7 @@
 import crypto from "crypto";
 
 // 26 * 2 + 10 = 62
-const chars = ["A", "a", "B", "b", "C", "c", "D", "d", "E", "e", "F", "f", "G", "g", "H", "h", "I", "i", "J", "j", "K", "k", "L", "l", "M", "m", "N", "n", "O", "o", "P", "p", "Q", "q", "R", "r", "S", "s", "T", "t", "U", "u", "V", "v", "W", "w", "X", "x", "Y", "y", "Z", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789";
 
 /**
  * Gets an identifier consisting of 6 alphanumeric characters given a URL. 
--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,16 @@
-const express = require('express')
-const app = express();
+const app = require('express')();
 const jsonParser = require('body-parser').json();
 const cors = require('cors')
 const { check, validationResult } = require('express-validator');
-const { getIdentifier } = require("./identifier"); 
+const { getIdentifier } = require("./identifier");
 
 var corsOptions = {
     origin: 'http://localhost:8888', // TODO get from env/docker
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
+}
 
 // TODO get from env/docker
 const PORT = 3000;
-const ADRESS = "http://localhost";
 
 const identifierToUrl = new Map();
 
@@ -24,27 +22,26 @@ app.post("/url", [
 ], (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() });
+        return res.status(422).json({ errors: errors.array() });
     }
 
-    const { body: { url } } = req; 
+    const { body: { url } } = req;
     console.info("YOu submitted ", url);
     // Check if in redis else create identifier
     const identifier = getIdentifier(url);
 
     identifierToUrl.set(identifier, url);
     // Save to redis
-    res.json({url, minifiedUrl: `${ADRESS}:${PORT}/${identifier}`});
+    return res.status(200).json({ url, identifier });
 });
 
-app.get("/:identifier", (req, res) => {
+app.get("/url/:identifier", (req, res) => {
     const { params: { identifier } } = req;
-    
-    // Check if in redis else
-    // return res.status(404).send("No match =(")
+
     console.info("identifier", identifier);
-    const redirectUrl = identifierToUrl.get(identifier);
-    return res.redirect(301, redirectUrl);
+    const url = identifierToUrl.get(identifier);
+    return url ? res.status(200).json({ url, identifier }) :
+        res.status(404).send("No match =(");
 });
 
-app.listen(PORT, () => console.info(`Listening on ${PORT}`));
+app.listen(PORT, () => console.info(`API listening on ${PORT}`));
